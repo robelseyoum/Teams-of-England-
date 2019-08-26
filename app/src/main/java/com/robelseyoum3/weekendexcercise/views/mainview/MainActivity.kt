d9c6847a7efbc2292787f9604d6c6d908d3c3c90
Robel <robelseyoum3@gmail.com>
2019-08-26 14:16:00 +0100

    creating onClickListener from MainAcitivty

--- app/src/main/java/com/robelseyoum3/weekendexcercise/views/mainview/MainActivity.kt
+++ app/src/main/java/com/robelseyoum3/weekendexcercise/views/mainview/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.robelseyoum3.weekendexcercise.views.mainview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.robelseyoum3.weekendexcercise.common.Constants
@@ -12,18 +13,37 @@ import retrofit2.Response
 import android.util.Log
 import android.view.View
 import com.robelseyoum3.weekendexcercise.R
+import com.robelseyoum3.weekendexcercise.views.sortview.BubbleActivity
+import com.robelseyoum3.weekendexcercise.views.teamview.TeamActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View?) {
 
+
+        when(view!!.id){
+
+            R.id.btn_team_league ->{
+                intent = Intent(this, TeamActivity::class.java)
+
+            }
+
+            R.id.btn_bubble_sort ->{
+                intent = Intent(this, BubbleActivity::class.java)
+            }
+
+        }
+            startActivity(intent)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn_bubble_sort.setOnClickListener(this)
+        btn_team_league.setOnClickListener(this)
 
 
         val teamRequest = RetrofitInstances().retrofitInstances.create(TeamRequest::class.java)
